a99d95eff1723d5d4834b65fe4b2683ac137d635
Nikita Titov <nekit94-08@mail.ru>
2020-12-07 23:10:36 +0300

    Update create_or_update_review_comment.sh

--- .ci/create_or_update_review_comment.sh
+++ .ci/create_or_update_review_comment.sh
@@ -36,7 +36,7 @@ if [[ $mode == "create" ]]; then
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: token $SECRETS_WORKFLOW" \
       "${GITHUB_SERVER_URL}/repos/${GITHUB_REPOSITORY}/pulls/$pr_number/comments" | \
-    jq --raw-output --arg comment_id "$comment_id" '.[] | select(.id|tostring == "$comment_id") | if has("in_reply_to_id") then .in_reply_to_id else .id end')
+    jq --raw-output --arg comment_id $comment_id '.[] | select(.id|tostring == $comment_id) | if has("in_reply_to_id") then .in_reply_to_id else .id end')
   data=$(jq -n \
     --arg body "$body" \
     '{"body":$body}')
