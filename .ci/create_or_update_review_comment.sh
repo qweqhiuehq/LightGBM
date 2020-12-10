7e72b7d6e42d30753dff1bba76c12dbc7f7a65fe
Nikita Titov <nekit94-08@mail.ru>
2020-12-10 20:19:08 +0300

    Update create_or_update_review_comment.sh

--- .ci/create_or_update_review_comment.sh
+++ .ci/create_or_update_review_comment.sh
@@ -54,7 +54,7 @@ elif [[ $mode == "append" ]]; then
     -H "Authorization: token $SECRETS_WORKFLOW" \
     "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls/comments/$comment_id" | \
     jq --raw-output '.body')
-  echo "$old_comment_body"
+  echo "$body"
   data=$(jq -n \
     --argjson body "\"$old_comment_body\r\n$body\"" \
     '{"body":$body}')
