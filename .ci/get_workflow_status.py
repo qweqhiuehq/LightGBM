281589eb67da5b7ccb52612bef296d6ae09dbf30
Nikita Titov <nekit94-08@mail.ru>
2020-12-13 21:54:31 +0300

    Update get_workflow_status.py

--- .ci/get_workflow_status.py
+++ .ci/get_workflow_status.py
@@ -8,46 +8,46 @@ except ImportError:
     import urllib2 as request
 
 
-def get_runs(workflow_id):
+def get_runs(workflow_name):
     pr_runs = []
     if environ.get("GITHUB_EVENT_NAME", "") == "pull_request":
-        req = request.Request(url="{}/repos/{}/actions/workflows/{}/runs".format(environ.get("GITHUB_API_URL"),
-                                                                                 environ.get("GITHUB_REPOSITORY"),
-                                                                                 workflow_id),
+        pr_number = int(environ.get("GITHUB_REF").split('/')[-2])
+        req = request.Request(url="{}/repos/{}/pulls/{}/comments?sort=created&direction=desc".format(environ.get("GITHUB_API_URL"),
+                                                                                                     environ.get("GITHUB_REPOSITORY"),
+                                                                                                     pr_number),
                               headers={"Accept": "application/vnd.github.v3+json"})
         url = request.urlopen(req)
         data = json.loads(url.read().decode('utf-8'))
         url.close()
-        pr_runs = [i for i in data['workflow_runs']
-                   if i['event'] == 'pull_request_review_comment' and
-                   (i.get('pull_requests') and
-                    i['pull_requests'][0]['number'] == int(environ.get("GITHUB_REF").split('/')[-2]) or
-                    i['head_branch'] == environ.get("GITHUB_HEAD_REF").split('/')[-1])]
-    return sorted(pr_runs, key=lambda i: i['run_number'], reverse=True)
+        pr_runs = [i for i in data
+                   if i['user']['id'] == 25141164
+                   and i['body'].startswith('Workflow **{}** has been triggered!'.format(workflow_name))]
+    return pr_runs
 
 
 def get_status(runs):
     status = 'ok'
     for run in runs:
-        if run['status'] == 'completed':
-            if run['conclusion'] == 'skipped':
+        body = run['body']
+        if "Status: " in body:
+            if "Status: skipped." in body:
                 continue
-            if run['conclusion'] in {'failure', 'timed_out', 'cancelled'}:
+            if "Status: failure ❌." in body:
                 status = 'fail'
                 break
-            if run['conclusion'] == 'success':
+            if "Status: success ✔️.":
                 status = 'ok'
                 break
-        if run['status'] in {'in_progress', 'queued'}:
+        else:  # in progress
             status = 'rerun'
             break
     return status
 
 
 if __name__ == "__main__":
-    workflow_id = argv[1]
+    workflow_name = argv[1]
     while True:
-        status = get_status(get_runs(workflow_id))
+        status = get_status(get_runs(workflow_name))
         if status != 'rerun':
             break
         sleep(60)
