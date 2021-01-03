910529b6a5f778d53fd225c78e5946749e2ed266
Nikita Titov <nekit94-08@mail.ru>
2021-01-03 18:11:55 +0300

    Update get_workflow_status.py

--- .ci/get_workflow_status.py
+++ .ci/get_workflow_status.py
@@ -27,7 +27,7 @@ def get_runs(workflow_name):
 
 
 def get_status(runs):
-    status = 'ok'
+    status = 'success'
     for run in runs:
         body = run['body']
         if "Status: " in body:
@@ -37,7 +37,7 @@ def get_status(runs):
                 status = 'fail'
                 break
             if "Status: success" in body:
-                status = 'ok'
+                status = 'success'
                 break
         else:  # in progress
             status = 'rerun'
